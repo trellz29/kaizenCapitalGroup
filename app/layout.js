45ae96694cadc4e470f919cfeafc00d0e493e79e
Cottrell Phillip <support@kaizencapitalgrp.com>
2026-05-28 22:10:19 -0400

    Add Tawk.to live chat widget

--- app/layout.js
+++ app/layout.js
@@ -29,7 +29,18 @@ export default function RootLayout({ children }) {
         gtag('js', new Date());
         gtag('config', 'G-7F3F4YLRLF');
       ` }} />
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">{children}      <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/6a18f4c2c95c7a1c33ced5d5/1jpono5pb';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+        })();
+      </script>
+      </body>
     </html>
   );
 }
